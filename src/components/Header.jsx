dac232cc80be96aed773b27289e62db66d632418
DavidCam87 <davidcamuneziglesias@gmail.com>
2024-09-16 01:35:20 +0200

    cambios Nav a NavlLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+
   );
 };
 
